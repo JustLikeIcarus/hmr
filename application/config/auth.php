98416cf9c7e26fb5035936a439e94c59a853abbc
root <root@x.vps9f6c1158.com>
2011-06-11 06:38:54 +0400

    first commit

---
+++ application/config/auth.php
@@ -0,0 +1,15 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+
+return array
+(
+	'driver' => 'ORM',
+	'hash_method' => 'sha256',
+	'hash_key'     => '87700DB755B5C9E6767641415841C419F92CED4887A40815F1ACFE0C2B094C28',
+	'salt_pattern' => '1, 3, 5, 9, 14, 15, 20, 21, 28, 30',
+	'lifetime' => 1209600,
+	'session_key' => 'auth_user',
+	'users' => array
+	(
+		// 'admin' => 'b3154acf3a344170077d11bdb5fff31532f679a1919e716a02',
+	),
+);
