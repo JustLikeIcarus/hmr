0560a3f85de7fb411a329e6e8e6388f982bc99d3
Kenny Kurtz <JustLikeIcarus@gmail.com>
2011-06-11 22:23:13 -0400

    fb

---
+++ application/config/facebook.php
@@ -0,0 +1,7 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+return array(
+   'app_id'          => '126029624146832',
+   'api_key'         => '7d851663d7260a552e829b30fa4db84a',
+   'secret'          => '0138a07a489858e3d55129907d611729',
+);
