79523dd18a9ffaea63681cc17603866b809cff90
Kenny Kurtz <JustLikeIcarus@gmail.com>
2011-06-11 22:35:34 -0400

    session

---
+++ application/config/session.php
@@ -0,0 +1,11 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+return array(
+	'native' => array(
+	        'name' => 'hmr_session',
+	        'lifetime' => 43200,
+	    ),
+	'cookie' => array(
+		'encrypted' => FALSE,
+	),
+);
