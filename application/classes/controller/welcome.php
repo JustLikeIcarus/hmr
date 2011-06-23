783b4c2f778bf7dbd1376d1a5391fb327723e1f4
Kenny <justlikeicarus@gmail.com>
2011-06-22 23:02:06 -0700

    Edited application/classes/controller/welcome.php via GitHub

--- application/classes/controller/welcome.php
+++ application/classes/controller/welcome.php
@@ -1,7 +1,8 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
 class Controller_Welcome extends Controller {
-
+	public $auth_required = 'login'; // FALSE | string | array
+	public $template = 'template/useradmin';
 	public function action_index()
 	{
 		$this->response->body('hello, world!');
